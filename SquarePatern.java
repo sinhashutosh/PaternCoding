608bcabecc67eb22d6b793f591e3d57d18ed8074
Ashutosh <ashu.cse.sln@gmail.com>
2022-09-29 16:23:26 +0530

    Square pattern outside one space inside one

---
+++ SquarePatern.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class SquarePatern {
+    public static void main(String[] args) {
+        int row = 5;
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < row; j++) {
+                System.out.print("* ");
+            }
+            System.out.println(" ");
+        }
+    }
+}
